61d71aa61d0fd1407b48c926e21b42a660397e4e
benrobson <shadowolfbusiness@gmail.com>
2023-08-02 21:23:50 +1000

    Implement draft configure command.

--- commands/configure.mjs
+++ commands/configure.mjs
@@ -1,28 +1,55 @@
 import { Command, RegisterBehavior } from '@sapphire/framework';
-import { getTimezones } from '../controller/common';
 
 export class ConfigureCommand extends Command {
   constructor(context, options) {
     super(context, {
       ...options,
-      description: 'Configure DevoteMe to apply changes to features.',
+      description: 'Configure to apply changes to features.',
       chatInputCommand: {
         register: true,
         behaviorWhenNotIdentical: RegisterBehavior.Overwrite
       },
     });
+
+    // Define subcommands
+    this.subcommands = [
+      {
+        name: 'timezone',
+        description: 'Configure the timezone.',
+      },
+      {
+        name: 'channel',
+        description: 'Configure the channel id for specific features to send to.',
+      },
+      // Add more subcommands here if needed
+    ];
   }
 
   registerApplicationCommands(registry) {
+    // Register main command
     registry.registerChatInputCommand((builder) =>
-      builder //
+      builder
         .setName(this.name)
         .setDescription(this.description)
     );
-  }
 
+    // Register subcommands
+    for (const subcommand of this.subcommands) {
+      registry.registerChatInputCommand((builder) =>
+        builder
+          .setName(subcommand.name)
+          .setDescription(subcommand.description)
+      );
+    }
+  }
 
   async chatInputRun(interaction) {
-    return interaction.reply(`Configure Panel`);
+    interaction.reply(`Use /${this.name} help for available subcommands.`);
   }
+
+  async timezoneSubcommand(interaction) {
+    interaction.reply(`Configure timezone logic`);
+  }
+
+  // Implement methods for other subcommands here if needed
 }
